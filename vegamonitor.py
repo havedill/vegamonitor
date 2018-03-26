0b3bd83ac439b994655ce2b6e7c546b807c43774
Jay Greco <jayv.greco@gmail.com>
2018-03-25 23:41:13 -0600

    Huge refactor
    fixed many bugs + added dev mode (see readme) + cleaned up code to better conform to PEP

--- vegamonitor.py
+++ vegamonitor.py
@@ -17,8 +17,26 @@ overdrivepath = cfg['global']['overdrivepath']
 overdriveargs = cfg['global']['overdriveargs']
 timethreshold = cfg['global']['timethreshold']
 hashthreshold = cfg['global']['hashthreshold']
+logsamples = cfg['global']['logsamples']
 pattern = "Totals:\s+[0-9]+\.[0-9]+\s([0-9]+).*$"
+
+applyoverdrive = cfg['actions']['applyoverdrive']
+restartdivers = cfg['actions']['restartdivers']
+
 restartreason = ""
+devMode = False
+
+metrics = {'hashrate': [], 'onlinetime': []}
+
+if 'test' in cfg['global']['context']:
+    print "!!! RUNNING IN DEVELOPMENT MODE !!!"
+    devMode = True
+
+if restartdivers:
+    print "Configured to reset drivers on restart."
+
+if applyoverdrive:
+    print "Configured to apply overdrive settings on restart."
 
 if 'XMR' in cfg['global']['app']:
     app = 'XMR'
@@ -30,19 +48,41 @@ if 'CAST' in cfg['global']['app']:
     app = 'CAST'
     path = cfg['castxmr']['path']
     procname=  cfg['castxmr']['procname']
-    castargs = cfg['castxmr']['castargs']
+    scriptname = cfg['castxmr']['scriptname']
     url = cfg['castxmr']['url']
 
-
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+    if devMode:
+        HEADER = ''
+        OKBLUE = ''
+        OKGREEN = ''
+        WARNING = ''
+        FAIL = ''
+        ENDC = ''
+        BOLD = ''
+        UNDERLINE = ''
+    else:
+        HEADER = '\033[95m'
+        OKBLUE = '\033[94m'
+        OKGREEN = '\033[92m'
+        WARNING = '\033[93m'
+        FAIL = '\033[91m'
+        ENDC = '\033[0m'
+        BOLD = '\033[1m'
+        UNDERLINE = '\033[4m'
+
+def warmupReached(arr):
+    return len(arr) >= logsamples
+
+def movingAverage(arr):
+    return sum(arr)/len(arr)
+
+def log(arr, sample):
+    arr.append(sample)
+
+    if len(arr) > logsamples:
+        #pop the oldest reading off the beginning of the list
+        arr.pop(0)
 
 def tail(filename, pattern, maxlines=60):
     global restartreason
@@ -55,16 +95,18 @@ def tail(filename, pattern, maxlines=60):
                 #I dont want to scan old logs. So break this loop if before we go too deep.
                 if lines > maxlines:
                     continue
+
                 if 'Totals' in l:
                     hashrate = re.search(pattern, l)
                     if hashrate:
                         return hashrate.group(1)
+
         print(bcolors.WARNING + "No 60s hash found yet. Waiting for that to appear.. ({})".format(waitcount) + bcolors.ENDC)
         waitcount += 1
         if waitcount > 31:
             print(bcolors.FAIL + "Waited too long for an average hash! Kill it all." + bcolors.ENDC)
             restartreason += "{} - Timeout waiting for 60s Hash".format(now)
-            restarttime()
+            restartMiner()
             waitcount = 0
         time.sleep(10)
 
@@ -75,20 +117,24 @@ def mtime(logfile, timethreshold):
     diff = (lastmtime - cutoff) / datetime.timedelta(minutes=1)
     if diff < (timethreshold / 2):
         print(bcolors.WARNING + "Potential restart incoming. {} minutes until we deem stale".format(diff) + bcolors.ENDC)
+
     else:
         print("Logfile timeout countdown {} minutes".format(diff))
+
     if lastmtime > cutoff:
         #The file is updating actively
+
         return True
     else:
+
         #File stopped updating.
         return False
 
-def stopprocess(procname):
+def stopProcess(procname):
     r = os.system('taskkill /f /im {}'.format(procname))
     print('Killing {} returned {}'.format(procname, r))
 
-def resetdrivers(devconpath):
+def resetDrivers(devconpath):
     #reset the drivers using devcon. Note this currently is using the CWD of the script, so it needs to be in the same location as this python script
     print('Resetting Drivers')
     r = subprocess.Popen('{} restart "PCI\VEN_1002&DEV_687F*"'.format(devconpath))
@@ -99,108 +145,144 @@ def overdrive(overdrivepath, overdriveargs):
     print('Applying Overdrive configs')
     r = subprocess.Popen('{} {}'.format(overdrivepath, overdriveargs), shell=True)
 
-def startmining(path, procname):
+def startMining(path, procname):
     if "XMR" in app:
         print('Spinning up XMR-stak\n')
         subprocess.Popen('start cmd /C "{}\{}"'.format(path, procname), shell=True)
+
     if "CAST" in app:
         print('Spinning up Cast-XMR\n')
-        subprocess.Popen('start cmd /C "{}\{} {}"'.format(path, procname, castargs), shell=True)
+        subprocess.Popen('start cmd /C "{}\{}"'.format(path, scriptname), shell=True)
 
-def restarttime():
-    stopprocess(procname)
-    time.sleep(4)
-    try:
-        os.remove(logfile)
-    except (OSError, NameError) as e:
+def restartMiner():
+    #clear the metrics
+    global metrics
+    metrics = {'hashrate': [], 'onlinetime': []}
+
+    if devMode:
         pass
-    resetdrivers(devconpath)
-    overdrive(overdrivepath, overdriveargs)
-    os.chdir(path)
-    startmining(path, procname)
-    print('Waiting 120 seconds to get new average hash rates...')
-    time.sleep(120)
 
-def xmrstakcheck():
+    else:
+        stopProcess(procname)
+        time.sleep(4)
+        try:
+            os.remove(logfile)
+
+        except (OSError, NameError) as e:
+            pass
+
+        if restartdivers:
+            resetDrivers(devconpath)
+
+        if applyoverdrive:
+            overdrive(overdrivepath, overdriveargs)
+
+        os.chdir(path)
+        startMining(path, procname)
+
+def checkEqual(lst):
+   return lst[1:] == lst[:-1]
+
+def XMRStakCheck():
     global restartreason
     if os.path.exists(logfile):
         currenthash = tail(logfile, pattern)
+        log(metrics['hashrate'], currenthash)
+
         updating = mtime(logfile, timethreshold)
-        if int(currenthash) < hashthreshold:
-            print(bcolors.FAIL + 'Hashrate of {} is below set threshold of {}! Resetting all miner settings'.format(currenthash, hashthreshold) + bcolors.ENDC)
-            restarttime()
-            restartreason += "\ns{} - Low Hashrate ({} H/s)".format(now, currenthash)
-        if updating == False:
-            print(bcolors.FAIL + 'The logfile ({}) hasn\'t been updating for {} minutes. Restart sequence beginning'.format(logfile, timethreshold) + bcolors.ENDC)
-            restarttime()
-            restartreason += "\n{} - Logfile timeout".format(now)
+
+        if not warmupReached(metrics['hashrate']):
+            #moving average is not accurate yet, so return.
+            print('SMA is not ready yet. Waiting...' + bcolors.ENDC)
+            return
+
+        else:
+            if movingAverage(metrics['hashrate']) < hashthreshold:
+                print(bcolors.FAIL + 'Hashrate of {}H/s is below set threshold of {}! Resetting all miner settings'.format(movingAverage(metrics['hashrate']), hashthreshold) + bcolors.ENDC)
+                restartreason += "\n{} - Low Hashrate ({} H/s)".format(now, movingAverage(metrics['hashrate']))
+                restartMiner()
+
+            if updating == False:
+                print(bcolors.FAIL + 'The logfile ({}) hasn\'t been updating for {} minutes. Restart sequence beginning'.format(logfile, timethreshold) + bcolors.ENDC)
+                restartreason += "\n{} - Logfile timeout".format(now)
+                restartMiner()
+
     print(bcolors.OKGREEN + 'Hashrate: {}H/s\nLog updating: {}\n'.format(currenthash, updating) + bcolors.ENDC)
 
-def castcheck():
+def castXMRCheck():
     global restartreason
+
     try:
         response = requests.get(url)
+
     except:
         print(bcolors.FAIL + 'Local webserver threw exception. Is CastXMR down? Restarting just in case.' + bcolors.ENDC)
-        restarttime()
-        restartreason += "{} - Webserver caught exception".format(now)
+        restartMiner()
+        restartreason += "{} - Webserver caught exception\n".format(now)
         return
+
     if response.status_code > 300:
         print(bcolors.FAIL + 'Local webserver returned {}. Is CastXMR down? Restarting just in case.'.format(response.status_code) + bcolors.ENDC)
-        restarttime()
+        restartMiner()
         restartreason += "{} - Web return {}".format(now, response.status_code)
+
     else:
         loaded = json.loads(response.text)
-        #interesting conversion. Perhaps this is why cast seems to have higher values than other miners?
         currenthash = loaded['total_hash_rate'] / 1000
+
         #tracker for 'online' minutes
         online = loaded['pool']['online']
 
-        if currenthash < hashthreshold or online == 0:
-            print(bcolors.WARNING + 'Hashrate of {}H/s is below threshold, or miner is offline. Grabbing a 60s average.'.format(currenthash) + bcolors.ENDC)
-            hash = 0
-            on = 0
-            for count in range(1, 7):
-                time.sleep(10)
-                #since i do 10 second intervals,
+        #use new SMA method
+        log(metrics['hashrate'], currenthash)
+        log(metrics['onlinetime'], online)
 
-                try:
-                    response = requests.get(url)
-                    loaded = json.loads(response.text)
+        if devMode:
+            print 'hashrate moving average:', movingAverage(metrics['hashrate'])
+            print 'hashrate array:', metrics['hashrate']
+            print 'online array:', metrics['onlinetime']
 
-                except:
-                    #assume exceptions are caused by issues anyways. Let it continue and restart
-                    pass
+        if not warmupReached(metrics['hashrate']):
+            #moving average is not accurate yet, so return.
+            print('SMA is not ready yet. Waiting...' + bcolors.ENDC)
+            return
 
-                h = loaded['total_hash_rate'] / 1000
-                o = loaded['pool']['online']
+        else:
+            if movingAverage(metrics['hashrate']) < hashthreshold:
+                print(bcolors.FAIL + 'SMA Hashrate is below set threshold! Resetting all miner settings' + bcolors.ENDC)
+                restartreason += "\n{} - Low Hashrate ({} H/s)".format(now, movingAverage(metrics['hashrate']))
+                restartMiner()
 
-                hash += h
-                on += o
-                print(bcolors.WARNING + 'Checking {}/6 hashrate: {}H/s'.format(count, h) + bcolors.ENDC)
-                print(bcolors.WARNING + 'Checking {}/6 online: {} seconds'.format(count, o) + bcolors.ENDC)
+            elif checkEqual(metrics['hashrate']):
+                print(bcolors.FAIL + 'Hashrate is the same across all samples! Resetting all miner settings'.format(currenthash, hashthreshold) + bcolors.ENDC)
+                restartreason += "\n{} - Identical hashrate -- likely CastXMR hang".format(now)
+                restartMiner()
 
-            #take the 60s average and fire a restart if it's below
-            if (hash/6) < hashthreshold:
-                print(bcolors.FAIL + 'Hashrate of {}H/s is below set threshold of {}! Resetting all miner settings'.format(currenthash, hashthreshold) + bcolors.ENDC)
-                restarttime()
-                restartreason += "\n{} - Low Hashrate ({} H/s)".format(now, currenthash)
-
-            if (on) == 0:
+            elif checkEqual(metrics['onlinetime']):
                 print(bcolors.FAIL + 'Online time has been 0 for the past minute! Resetting all miner settings'.format(currenthash, hashthreshold) + bcolors.ENDC)
-                restarttime()
                 restartreason += "\n{} - Online time of zero -- likely CastXMR hang".format(now)
-                
+                restartMiner()
+
         print(bcolors.OKGREEN + 'Hashrate: {}H/s\nWeb request returns: {}\n'.format(currenthash, response.status_code) + bcolors.ENDC)
 
-while True:
-    print(bcolors.BOLD + '\n\n==============\n' + bcolors.ENDC)
-    now = datetime.datetime.now()
-    if "XMR" in app:
-        xmrstakcheck()
-    if "CAST" in app:
-        castcheck()
-    if restartreason:
-        print(bcolors.BOLD + '======Reasons for Restarts======' + bcolors.ENDC)
-        print(bcolors.WARNING + '{}\n'.format(restartreason) + bcolors.ENDC)
-    time.sleep(10)
+if __name__ == '__main__':
+    while True:
+        print(bcolors.BOLD + '\n\n==============\n' + bcolors.ENDC)
+        now = datetime.datetime.now()
+
+        if "XMR" in app:
+            XMRStakCheck()
+
+        if "CAST" in app:
+            castXMRCheck()
+
+        if restartreason and warmupReached(metrics['hashrate']):
+            print(bcolors.BOLD + '======Reasons for Restarts======' + bcolors.ENDC)
+            print(bcolors.WARNING + '{}\n'.format(restartreason) + bcolors.ENDC)
+
+        if devMode:
+            #might as well speed this up
+            time.sleep(timethreshold/5)
+
+        else:
+            time.sleep(timethreshold)
